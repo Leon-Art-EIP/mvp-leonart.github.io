823c7c561a06a911bf3abbdcaf9fe5c2746511fd
DamienDemontis <damien.demontis@epitech.eu>
2022-12-07 20:22:34 +0100

    [ADD] added package.json dependencies, carroussel working but import fail

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,18 +1,64 @@
 import { Row } from "antd";
 import styled from "styled-components";
+import "./styles.css";
+import Carousel from "react-spring-3d-carousel";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import Card from "./Card";
+import CardNews from "./CardNews";
+import { config } from "react-spring";
+import Carroussel from "./Carroussel";
 
 export default function Home() {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews />
+    }
+  ];
+
   return (
     <HomeWrapper>
-      <StyledSloganRow align="middle" justify="center">
-        <StyledSlogan>Ça va se vendre comme des petits peintres !</StyledSlogan>
-      </StyledSloganRow>
+      <Carroussel
+        cards={cards}
+        height="500px"
+        width="90%"
+        margin="0 auto"
+        offset={2}
+        showArrows={false}
+      />
     </HomeWrapper>
   );
 }
 
 const HomeWrapper = styled.div`
   height: 100vh;
+  padding-top: 100px;
+  padding-top: 150px;
   background: linear-gradient(#d8f3dc, #2d6a4f);
 `;
 
