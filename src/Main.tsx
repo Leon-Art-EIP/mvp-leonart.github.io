6bceee46728cb4f1f68198b3ce9142d3ede1b374
MariusNWK <marius.nowak@epitech.eu>
2022-12-07 17:46:05 +0100

    fix: searchbar displayed only in Home component

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
-import Searchbar from "./Searchbar/Searchbar";
 import Pages from "./Pages/Pages";
 
 export default function Main() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Searchbar />
       <Pages />
     </BrowserRouter>
   );
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,10 @@
-import { Row } from "antd";
 import styled from "styled-components";
+import Searchbar from "../../Searchbar/Searchbar";
 
 export default function Home() {
   return (
     <HomeWrapper>
-      <StyledSloganRow align="middle" justify="center">
-        <StyledSlogan>Ça va se vendre comme des petits peintres !</StyledSlogan>
-      </StyledSloganRow>
+      <Searchbar />
     </HomeWrapper>
   );
 }
@@ -14,15 +12,4 @@ export default function Home() {
 const HomeWrapper = styled.div`
   height: 100vh;
   background: linear-gradient(#d8f3dc, #2d6a4f);
-`;
-
-const StyledSloganRow = styled(Row)`
-  width: 100%;
-  height: 100%;
-`;
-
-const StyledSlogan = styled.div`
-  color: green;
-  font-family: "Lobster", cursive;
-  font-size: xx-large;
 `;
